bcf850ad558db9357365a72629e5f9c37f49b0cc
RoaaA <RoaaA@DESKTOP-OH14MPH>
2021-07-05 10:32:22 +0300

    Add project files.

---
+++ WebApplication12/Controllers/StudentController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication12.Models;
+
+namespace WebApplication12.Controllers
+{
+    public class StudentController : Controller
+    {
+        IList<Student> studentList = new List<Student>() {
+                    new Student(){ StudentId=1, StudentName="John", Age = 18 },
+                    new Student(){ StudentId=2, StudentName="Steve", Age = 21 },
+                    new Student(){ StudentId=3, StudentName="Bill", Age = 25 },
+                    new Student(){ StudentId=4, StudentName="Ram", Age = 20 },
+                    new Student(){ StudentId=5, StudentName="Ron", Age = 31 },
+                    new Student(){ StudentId=6, StudentName="Chris", Age = 17 },
+                    new Student(){ StudentId=7, StudentName="Rob", Age = 19 }
+                };
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View(studentList.OrderBy(s => s.StudentId).ToList());
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            //here, get the student from the database in the real application
+
+            //getting a student from collection for demo purpose
+            var std = studentList.Where(s => s.StudentId == Id).FirstOrDefault();
+
+            return View(std);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student std)
+        {
+            if (ModelState.IsValid)
+            { //checking model state
+
+                //update student to db
+
+                return RedirectToAction("Index");
+            }eppo[]
+            return View(std);
+        }
+    }
+}
